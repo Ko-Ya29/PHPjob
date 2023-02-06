e50b4ed3a5c40ebe75500f52d92a41cd422f2fe5
Ko-Ya29 <122602121+Ko-Ya29@users.noreply.github.com>
2023-02-06 13:09:56 +0900

    3-3 提出
    修正したので，確認をお願いします。

--- 3-3/3-3 result.php
+++ 3-3/3-3 result.php
@@ -1,20 +1,21 @@
 <?php
 
 $number = $_POST['number'];
-$key = str_split($number);
+$arr = str_split($number);
+$key = array_rand($arr, 1);
 
 ?>
 
 <p><?php echo date("Y/m/d", time()); ?>の運勢は</p>
-<p>選ばれた数字は<?php echo $number[$key]; ?></p>
+<p>選ばれた数字は<?php echo $arr[$key]; ?></p>
 <p><?php
-    if ($number[$key] = 0) {
+    if ($arr[$key] == 0) {
         echo "凶";
-    } elseif ($number[$key] <= 3) {
+    } elseif ($arr[$key] <= 3) {
         echo "小吉";
-    } elseif ($number[$key] <= 6) {
+    } elseif ($arr[$key] <= 6) {
         echo "中吉";
-    } elseif ($number[$key] <= 8) {
+    } elseif ($arr[$key] <= 8) {
         echo "吉";
     } else {
         echo "大吉";    
